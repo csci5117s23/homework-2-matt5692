06b3ca49993ef1c773115e947aad5357f0b32807
letel005 <letel005@umn.edu>
2023-04-05 13:49:46 -0500

    delete placeholder api and setup codehooks

---
+++ homework-2/backend/homework2/index.js
@@ -0,0 +1,18 @@
+
+/*
+* Auto generated Codehooks (c) example
+* Install: npm i codehooks-js codehooks-crudlify
+*/
+import {app} from 'codehooks-js'
+import {crudlify} from 'codehooks-crudlify'
+
+//test route
+app.get("/test", (req, res) => {
+    res.json({"message": "you did it!"});
+});
+
+// Use Crudlify to create a REST API for any collection
+crudlify(app)
+
+// bind to serverless runtime
+export default app.init();
